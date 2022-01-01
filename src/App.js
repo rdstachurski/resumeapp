376163b27fc861fb60ad49581188b27fa482c379
rdstachurski <robertdstachurski@gmail.com>
2022-01-01 16:59:04 -0500

    added a picture

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import {hot} from "react-hot-loader";
 import React from 'react';
+import image from './img/gun.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="App-header">Welcome to my Website! Please hire me!</h1>
-      <h2>Robert Stachurski</h2>
+      <h1 className="App-header">Welcome to my Website! Please hire me!
+         <h2>Robert Stachurski</h2>
+         <img src={image} alt ="gun-image"/>
+      </h1>
+
     </div>
   );
 }
